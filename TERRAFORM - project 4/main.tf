36081d54988cab2eddd40dbc596f58e7c14688db
Rahaman <rahaman.abdoul786@gmail.com>
2024-09-27 11:43:06 +0200

    ver1.3 - project 4

---
+++ TERRAFORM - project 4/main.tf
@@ -0,0 +1,17 @@
+﻿variable "ami" {}
+variable "region" {}
+variable "instance_type" {}
+variable "access_key" {}
+variable "secret_key" {}
+variable "subnet_id" {}
+
+
+# Création de l'instance EC2
+resource "aws_instance" "web-terraform-cloud" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+  tags = {
+    Name = "self.id"
+  }
+}
